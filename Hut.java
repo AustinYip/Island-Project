636829004a8826048eea80fe3e067804531794e5
AustinYip <101162675+AustinYip@users.noreply.github.com>
2022-11-08 14:49:49 -0500

    Add files via upload

--- Hut.java
+++ Hut.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.ArrayList;
 /**
  * Write a description of class Hut here.
  * 
@@ -9,24 +9,30 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Hut extends Structure
 {
     private int peopleInHut;
-    private boolean hutFull;
+    //private boolean hutFull;
     private GreenfootImage hutImage = new GreenfootImage("hut.png");
-
+    private ArrayList residence;
+    private int adultCount;
+    private boolean reproducable;
     public GreenfootImage resetImage(){
         return new GreenfootImage (hutImage);
     }
 
     public Hut(){
         peopleInHut = 0;
-        hutFull = false;
+        //hutFull = false;
+        ArrayList<Person> residence = new ArrayList<Person>();
     }
 
     public void run(){
-        if(peopleInHut >= 2){
-            hutFull = true;
+        for (int i = 0; i < residence.size(); i++){
+            if (residence.get(i) instanceof Adult){
+                adultCount++;
+            }
         }
     }
     
+    /*
     private void addPeople(){
         if (hutFull == false){
             peopleInHut++;
@@ -35,7 +41,15 @@ public class Hut extends Structure
             }
         }
     }
+    */
     
+    private void enterHouse(Person p){
+        residence.add(p);
+    }
+
     private void procreate(){
-        
+        if (adultCount>=2){
+            
+        }
+    }
 }
--- LivingThing.java
+++ LivingThing.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class StaticThing here.
+ * Write a description of class LivingThing here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public abstract class StaticThing extends ScrollableActor
+public abstract class LivingThing extends ScrollableActor
 {
     /**
-     * Act - do whatever the StaticThibng wants to do. This method is called whenever
+     * Act - do whatever the LivingThing wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
